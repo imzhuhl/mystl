567552dac4f35337f72d248021c4ce45fd46dd5a
zhuhonglinX <zhuhl1996@gmail.com>
2021-02-26 21:54:06 +0800

    update

--- SSTL/test.cpp
+++ SSTL/test.cpp
@@ -1,10 +1,14 @@
 //
 // Created by zhuhonglin on 2021/2/26.
 //
-
+#include <iostream>
 #include "vector.h"
 
 int main() {
-    return 0;
+    /********vector*********/
+    sstl::vector<int> v1;
+    sstl::vector<int> v2(10, 1);
+    std::cout << v2.size() << std::endl;
+
 }
 
--- SSTL/vector.h
+++ SSTL/vector.h
@@ -9,73 +9,105 @@
 #include <cstddef>
 #include <algorithm>
 
+#include "utils.h"
+
 namespace sstl{
 
-class vector {
-private:
-    int *begin_;
-    int *end_;
-    int *cap_;
+    template <typename T>
+    class vector {
+    private:
+        T* begin_;
+        T* end_;
+        T* cap_;
 
-    void init_space(size_t cap);
-    void fill_n(int* first, size_t n, int value);
+        void init_space(size_t n, size_t cap);
+        void fill_n(T* first, size_t n, T value);
+        void destroy_and_recover(T* first, T* last, size_t n);
 
-public:
-    vector() noexcept;
-    explicit vector(size_t n);
-    vector(size_t n, int value);
-    ~vector();
+    public:
+        vector() noexcept {
+            init_space(0, 16);
+        }
+        explicit vector(size_t n) {
+            vector(n, 0);
+        }
+        vector(size_t n, T value) {
+            const size_t init_size = std::max(static_cast<size_t>(16), n);
+            init_space(n, init_size);
+            fill_n(begin_, n, value);
+        }
+        ~vector() {
+            destroy_and_recover(begin_, end_, cap_ - begin_);
+            begin_ = end_ = cap_ = nullptr;
+        }
 
-    vector& operator=(const vector& rhs);
-    vector& operator=(vector&& rhs) noexcept;
-    vector& operator=(std::initializer_list<int> ilist);
+        vector& operator=(const vector& rhs);
+        vector& operator=(vector&& rhs) noexcept;
+        vector& operator=(std::initializer_list<T> ilist);
 
-    // 迭代相关
-    int* begin() noexcept {return begin_;}
-    int* end() noexcept {return end_;}
+        // 迭代相关
+        T* begin() noexcept {return begin_;}
+        T* end() noexcept {return end_;}
 
-    // 容量相关
-    bool empty() const noexcept {
-        return begin_ == end_;
+        // 容量相关
+        bool empty() const noexcept {
+            return begin_ == end_;
+        }
+        size_t size() const noexcept {
+            return static_cast<size_t>(end_ - begin_);
+        }
+        size_t capacity() const noexcept {
+            return static_cast<size_t>(cap_ - begin_);
+        }
+
+        // 访问元素
+        T operator[](size_t n) {
+            assert(n < size());
+            return *(begin_ + n);
+        }
+        T front() {
+            assert(!empty());
+            return *begin_;
+        }
+        T back() {
+            assert(!empty());
+            return *(end_-1);
+        }
+
+        // 插入删除
+        void push_back(const T& value);
+        void push_back(T&& value);
+        void pop();
+
+
+    };
+
+
+    /******************************* helper func *******************************/
+    template <typename T> void vector<T>::init_space(size_t n, size_t cap) {
+        try {
+            begin_ = static_cast<T*>(::operator new(cap*sizeof(T)));
+            end_ = begin_ + n;
+            cap_ = begin_ + cap;
+        } catch (...) {
+            begin_ = nullptr;
+            end_ = nullptr;
+            cap_ = nullptr;
+        }
     }
 
-};
-
-vector::vector() noexcept {
-    init_space(16);
-}
-
-vector::vector(size_t n) {
-    vector(n, 0);
-}
-
-vector::vector(size_t n, int value) {
-    const size_t init_size = std::max(static_cast<size_t>(16), n);
-    init_space(init_size);
-    fill_n(begin_, n, value);
-}
-
-
-/******************************* helper func *******************************/
-void vector::init_space(size_t cap) {
-    try {
-        begin_ = static_cast<int*>(::operator new(cap*sizeof(int)));
-        end_ = begin_;
-        cap_ = begin_ + cap;
-    } catch (...) {
-        begin_ = nullptr;
-        end_ = nullptr;
-        cap_ = nullptr;
+    template <typename T> void vector<T>::fill_n(T *first, size_t n, T value) {
+        for (; n > 0; --n, ++first) {
+            *first = value;
+        }
     }
-}
 
-
-void vector::fill_n(int *first, size_t n, int value) {
-    for (; n > 0; --n, ++first) {
-        *first = value;
+    template <typename T> void vector<T>::destroy_and_recover(T* first, T* last, size_t n) {
+        destory(first, last);
+        if (first != nullptr) {
+            ::operator delete(first);
+        }
     }
-}
-
 
 }
 
